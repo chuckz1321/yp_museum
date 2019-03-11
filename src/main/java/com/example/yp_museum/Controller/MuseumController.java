ddc8f8590ea5184f6ca9b6803cc36c0df69b9eb5
mdudycz <mdudycz@uwo.ca>
2019-03-11 19:55:52 -0400

    Edited comments and word corrections

--- src/main/java/com/example/yp_museum/Controller/MuseumController.java
+++ src/main/java/com/example/yp_museum/Controller/MuseumController.java
@@ -18,11 +18,6 @@ public class MuseumController {
     @Autowired
     MuseumSvcImpl svc;
 
-   /* public abstract List<Museum> getMuseumListByState(String state);
-    public abstract List<Museum> getMuseumListByAddress(String address);
-    public abstract List<Museum> getMuseumListByCity(String city);
-    public abstract List<Museum> getMuseumListByName(String museumName);*/
-
     @RequestMapping(value="/test",method=RequestMethod.GET)
     @ResponseBody
     public ResponseMessage<List<Museum>> test(@RequestParam("country") String country){
